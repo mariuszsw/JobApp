785d349e9409f1fec16b787b9a5afba50ab179c6
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2025-04-22 00:19:52 +0200

    updated axios endpoint and added cors support for react

--- backend/src/main/java/com/mariusz/jobapp/JobRestController.java
+++ backend/src/main/java/com/mariusz/jobapp/JobRestController.java
@@ -3,12 +3,14 @@ package com.mariusz.jobapp;
 import com.mariusz.jobapp.model.JobPost;
 import com.mariusz.jobapp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobRestController {
 
     @Autowired
