0bd0e568ae51a6485d925313db8480a9d2660523
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2025-04-21 22:10:11 +0200

    add rest endpoint for retrieving job posts

--- backend/src/main/java/com/mariusz/jobapp/JobRestController.java
+++ backend/src/main/java/com/mariusz/jobapp/JobRestController.java
@@ -3,15 +3,20 @@ package com.mariusz.jobapp;
 import com.mariusz.jobapp.model.JobPost;
 import com.mariusz.jobapp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class JobRestController {
 
+    @Autowired
+    public JobService jobService;
+
+    @GetMapping("jobPosts")
+    public List<JobPost> getAllJobs() {
+        return jobService.getAllJobs();
+    }
 
 }
