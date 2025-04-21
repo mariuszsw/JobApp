c7a977ed306d1678faf2f88858be55513a3e7705
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2025-04-21 21:42:10 +0200

    refactoring code

---
+++ backend/src/main/java/com/mariusz/jobapp/JobRestController.java
@@ -0,0 +1,17 @@
+package com.mariusz.jobapp;
+
+import com.mariusz.jobapp.model.JobPost;
+import com.mariusz.jobapp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class JobRestController {
+
+
+}
